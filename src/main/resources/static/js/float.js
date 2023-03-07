152b2d1600c633ad11cbf983a74d1abd8f330032
hoey94 <cnnqjban521@gmail.com>
2023-03-07 13:07:53 +0800

    添加拖动js

--- src/main/resources/static/js/float.js
+++ src/main/resources/static/js/float.js
@@ -14,3 +14,25 @@ logo.addEventListener('click', function() {
     floatBoxLogo.style.backgroundColor = colors[index];
     sendBut.style.color = colors[index];
 });
+
+
+var box = document.querySelector(".floating-box");
+var isDragging = false;
+var mouseOffset = { x: 0, y: 0 };
+
+box.addEventListener("mousedown", function(event) {
+    isDragging = true;
+    mouseOffset.x = event.clientX - box.offsetLeft;
+    mouseOffset.y = event.clientY - box.offsetTop;
+});
+
+box.addEventListener("mouseup", function(event) {
+    isDragging = false;
+});
+
+box.addEventListener("mousemove", function(event) {
+    if (isDragging) {
+        box.style.left = (event.clientX - mouseOffset.x) + "px";
+        box.style.top = (event.clientY - mouseOffset.y) + "px";
+    }
+});
